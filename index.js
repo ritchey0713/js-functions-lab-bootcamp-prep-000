95e57652acc635c734a8beb81322355118830b1f
david ritchey <ritchey0713@gmail.com>
2018-03-01 21:43:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function happyHolidaysTo(name){
   return `Happy Holidays ${name}!`
 }
 
+function happyHolidaysTo(name, holiday){
+  return ` Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`
+}
